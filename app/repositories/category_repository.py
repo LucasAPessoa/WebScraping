d93def47e2ee1375899ea51a796f1d20a2e77e34
Lucas <lucasapessoa@outlook.com>
2025-05-14 14:23:32 -0300

    Refatora o método create_category para usar model_dump para criar a category  e renomeia o import do model de category para category_model

--- app/repositories/category_repository.py
+++ app/repositories/category_repository.py
@@ -1,5 +1,5 @@
 from sqlmodel import Session
-from app.models.category import Category
+from app.models.category_model import Category
 from app.schemas.category.category_schema import CategoryCreate
 
 class CategoryRepository:   
@@ -7,7 +7,7 @@ class CategoryRepository:
         self.session = session
 
     def create_category(self, category_create: CategoryCreate) -> Category:
-        category = Category.model_validate(category_create)
+        category = Category(**category_create.model_dump())
         self.session.add(category)
         self.session.commit()
         self.session.refresh(category)
