4a1c294ed7de6de13d2bfa09ff306ba3db8a10d5
Lucas <lucasapessoa@outlook.com>
2025-05-14 14:19:02 -0300

    Renomeia o arquivo category da pasta model para category_model

---
+++ app/models/category_model.py
@@ -0,0 +1,7 @@
+from sqlmodel import SQLModel, Field
+from uuid import UUID, uuid4
+from pydantic import constr
+
+class Category(SQLModel, table=True):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    name: str = Field(index=True, nullable=False)
