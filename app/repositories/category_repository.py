5629c67b16d39c29641b45e7c3f788ed2a17556d
Lucas <lucasapessoa@outlook.com>
2025-05-15 09:04:05 -0300

    adiciona método update_category ao repositório de categorias

--- app/repositories/category_repository.py
+++ app/repositories/category_repository.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlmodel import Session
 
 from app.schemas.category.category_schema import CategoryCreate, CategoryRead
@@ -9,6 +10,18 @@ class CategoryRepository:
     def create_category(self, category_create: CategoryCreate) -> CategoryRead:
         category = CategoryRead(**category_create.model_dump())
         self.session.add(category)
+        self.session.commit()
+        self.session.refresh(category)
+        return category
+    
+    def update_category(self, category_id: str, category_update: CategoryCreate) -> CategoryRead:
+        category = self.session.get(CategoryRead, category_id)
+        if not category:
+            raise HTTPException(status_code=404, detail="Category not found")
+        
+        for key, value in category_update.model_dump().items():
+            setattr(category, key, value)
+        
         self.session.commit()
         self.session.refresh(category)
         return category
