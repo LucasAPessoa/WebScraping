3475fc3d387de7e26fe332a353f4d4928b484696
Lucas <lucasapessoa@outlook.com>
2025-05-15 09:02:48 -0300

    Remove APIRoutere e ajusta o método create_category

--- app/controllers/category_controller.py
+++ app/controllers/category_controller.py
@@ -1,12 +1,11 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import Depends
 from sqlmodel import Session
 from app.schemas.category.category_schema import CategoryCreate, CategoryRead
 from app.services.category_services import CategoryService
 from app.database.session import get_session
 
-router = APIRouter(prefix="/categories", tags=["Categories"])
 
-@router.post("/", response_model=CategoryRead, status_code=status.HTTP_201_CREATED)
-def create_category(category_create: CategoryCreate, session: Session = Depends(get_session)):
+
+def create_category(category_create: CategoryCreate, session: Session):
     service = CategoryService(session)
     return service.create_category(category_create)
