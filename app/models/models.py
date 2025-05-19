3ace26ffc611716ae401d568ed9aa993c12f8afb
Lucas <lucasapessoa@outlook.com>
2025-05-18 23:27:54 -0300

    renomeia o arquivo category_models para models e centraliza todos os models em apenas um arquivo

---
+++ app/models/models.py
@@ -0,0 +1,39 @@
+from sqlmodel import SQLModel, Field
+from uuid import UUID, uuid4
+
+class Category(SQLModel, table=True):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    name: str = Field(index=True, nullable=False)
+    
+class Promotion(SQLModel, table=True):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    name: str = Field(index=True, nullable=False)
+
+class Link(SQLModel, table=True):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    url: str = Field(index=True, nullable=False)
+    
+class Establishment(SQLModel, table=True):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    name: str = Field(index=True, nullable=False)
+    photo_id: UUID = Field(foreign_key="photo.id")
+    links_id: UUID = Field(foreign_key="link.id")
+    
+class Photo(SQLModel, table=True):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    url: str = Field(index=True, nullable=False)
+    product_id: UUID = Field(foreign_key="product.id")
+    
+class Product_Placeholder(SQLModel, table=True):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    name: str = Field(index=True, nullable=False)
+    description: str = Field(index=True, nullable=False)
+    category_id: UUID = Field(foreign_key="category.id")
+    
+class Product(SQLModel, table=True):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    original_price: float = Field(index=True, nullable=False)
+    discounted_price: float = Field(index=True, nullable=False)
+    product_placeholder_id: UUID = Field(foreign_key="product_placeholder.id")
+    establishment_id: UUID = Field(foreign_key="establishment.id")
+    promotion_id: UUID = Field(foreign_key="promotion.id")
