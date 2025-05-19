118580313a3d3af5795c6e04fd43c9081cc05891
Lucas <lucasapessoa@outlook.com>
2025-05-18 23:28:26 -0300

    adiciona o import de todos os models do bd

--- app/database/session.py
+++ app/database/session.py
@@ -1,10 +1,21 @@
 from sqlmodel import create_engine, Session, SQLModel
 
+from app.models.models import (
+    Category,
+    Promotion,
+    Link,
+    Establishment,
+    Photo,
+    Product_Placeholder,
+    Product
+)
+
 DATABASE_URL = "postgresql+psycopg2://postgres:teste123@localhost:5432/webscraper"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
 def create_db_and_tables():
+    print("Criando tabelas...")
     SQLModel.metadata.create_all(engine)
 
 def get_session():
