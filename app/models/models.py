afd2f9c8c4422902794b438e967bab6549411b46
Lucas <lucasapessoa@outlook.com>
2025-05-24 08:53:56 -0300

    refatora modelos de estabelecimento e foto para corrigir referências de chave estrangeira e adicionar campos ausentes

--- app/models/models.py
+++ app/models/models.py
@@ -16,24 +16,26 @@ class Link(SQLModel, table=True):
 class Establishment(SQLModel, table=True):
     id: UUID = Field(default_factory=uuid4, primary_key=True)
     name: str = Field(index=True, nullable=False)
-    photo_id: UUID = Field(foreign_key="photo.id")
+    url: str = Field(index=True)
     links_id: UUID = Field(foreign_key="link.id")
     
 class Photo(SQLModel, table=True):
     id: UUID = Field(default_factory=uuid4, primary_key=True)
     url: str = Field(index=True, nullable=False)
-    product_id: UUID = Field(foreign_key="product.id")
+    product_id: UUID = Field(foreign_key="product_placeholder.id")
     
 class Product_Placeholder(SQLModel, table=True):
     id: UUID = Field(default_factory=uuid4, primary_key=True)
     name: str = Field(index=True, nullable=False)
     description: str = Field(index=True, nullable=False)
     category_id: UUID = Field(foreign_key="category.id")
+    metacritic_score: float = Field(index=True, nullable=False)
     
 class Product(SQLModel, table=True):
     id: UUID = Field(default_factory=uuid4, primary_key=True)
     original_price: float = Field(index=True, nullable=False)
     discounted_price: float = Field(index=True, nullable=False)
+    percentage_discount: float = Field(index=True, nullable=False)
     product_placeholder_id: UUID = Field(foreign_key="product_placeholder.id")
     establishment_id: UUID = Field(foreign_key="establishment.id")
     promotion_id: UUID = Field(foreign_key="promotion.id")
