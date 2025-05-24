c3aa574347088b93cc92eef580143e182e4370d4
Lucas <lucasapessoa@outlook.com>
2025-05-24 08:57:59 -0300

    adiciona a propriedade percentage_discount à classe Product para calcular o desconto percentual

--- app/models/models.py
+++ app/models/models.py
@@ -35,7 +35,12 @@ class Product(SQLModel, table=True):
     id: UUID = Field(default_factory=uuid4, primary_key=True)
     original_price: float = Field(index=True, nullable=False)
     discounted_price: float = Field(index=True, nullable=False)
-    percentage_discount: float = Field(index=True, nullable=False)
     product_placeholder_id: UUID = Field(foreign_key="product_placeholder.id")
     establishment_id: UUID = Field(foreign_key="establishment.id")
     promotion_id: UUID = Field(foreign_key="promotion.id")
+    
+    @property
+    def percentage_discount(self) -> float:
+        if self.original_price == 0:
+            return 0.0
+        return round((1 - self.discounted_price / self.original_price) * 100, 2)
