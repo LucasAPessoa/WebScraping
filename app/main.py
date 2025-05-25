6afe53c355c656e372a277937a05497c9c77d576
Lucas <lucasapessoa@outlook.com>
2025-05-25 20:22:43 -0300

    adiciona inclusão do roteador de estabelecimentos no aplicativo FastAPI

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.routes.category_routes import category_router
 from app.routes.promotion_routes import promotion_router
 from app.routes.link_routes import link_router
+from app.routes.establishment_routes import establishment_router
 from app.database.session import create_db_and_tables
 from contextlib import asynccontextmanager
 
@@ -17,3 +18,4 @@ app = FastAPI()
 app.include_router(category_router)
 app.include_router(promotion_router)
 app.include_router(link_router)
+app.include_router(establishment_router)
