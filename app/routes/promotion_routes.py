9c3d41d160f2a424a1cd2d9368721613f961b52d
Lucas <lucasapessoa@outlook.com>
2025-05-24 22:37:28 -0300

    adiciona rotas para manipulação de promoções: criação, atualização, exclusão e obtenção por ID

--- app/routes/promotion_routes.py
+++ app/routes/promotion_routes.py
@@ -13,3 +13,32 @@ def create_promotion(
     session: Session = Depends(get_session)
 ):
     return promotion_controller.create_promotion(promotion_create, session)
+
+@promotion_router.put("/{promotion_id}", response_model=PromotionRead, status_code=status.HTTP_200_OK)
+def update_promotion(
+    promotion_id: str, 
+    promotion_update: PromotionUpdate, 
+    session: Session = Depends(get_session)
+):
+    return promotion_controller.update_promotion(promotion_id, promotion_update, session)
+
+@promotion_router.delete("/{promotion_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_promotion(
+    promotion_id: str, 
+    session: Session = Depends(get_session)
+):
+    return promotion_controller.delete_promotion(promotion_id, session)
+
+@promotion_router.get("/{promotion_id}", response_model=PromotionRead, status_code=status.HTTP_200_OK)
+def get_promotion_by_id(
+    promotion_id: str, 
+    session: Session = Depends(get_session)
+):
+    return promotion_controller.get_promotion_by_id(promotion_id, session)
+
+@promotion_router.get("/", response_model=PromotionReadList, status_code=status.HTTP_200_OK)
+def get_all_promotions(
+    session: Session = Depends(get_session)
+):
+    return promotion_controller.get_all_promotions(session)
+
