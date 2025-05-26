9b7c2e7f756a5fea242033fe00288e1d88d34866
Lucas <lucasapessoa@outlook.com>
2025-05-26 10:45:45 -0300

    adiciona importação do roteador de fotos: inclui o roteador para gerenciamento de fotos no aplicativo

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.routes.link_routes import link_router
 from app.routes.establishment_routes import establishment_router
 from app.routes.plataform_routes import plataform_router
 from app.routes.product_placeholder_routes import product_placeholder_router
+from app.routes.photo_routes import photo_router
 from app.database.session import create_db_and_tables
 from contextlib import asynccontextmanager
 
@@ -23,3 +24,4 @@ app.include_router(link_router)
 app.include_router(establishment_router)
 app.include_router(plataform_router)
 app.include_router(product_placeholder_router)
+app.include_router(photo_router)
