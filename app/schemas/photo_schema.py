6db40b0ca08d6c535c548a1ffc51cebabfb934b5
Lucas <lucasapessoa@outlook.com>
2025-05-26 10:45:40 -0300

    adiciona schemas para gerenciamento de fotos: define modelos para criação, atualização e leitura de fotos

---
+++ app/schemas/photo_schema.py
@@ -0,0 +1,24 @@
+from uuid import UUID
+from typing import List
+from pydantic import BaseModel
+
+class PhotoCreate(BaseModel):
+    url: str
+    product_id: UUID
+
+class PhotoUpdate(BaseModel):
+    url: str
+
+class PhotoRead(BaseModel):
+    id: UUID
+    url: str
+    product_id: UUID
+
+    class Config:
+        from_attributes = True
+
+class PhotoReadList(BaseModel):
+    photos: List[PhotoRead]
+
+    class Config:
+        from_attributes = True
