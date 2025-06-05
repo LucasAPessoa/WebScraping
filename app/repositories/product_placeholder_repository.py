caf9265285212d50ad16c142b124a5c068e03ae1
Lucas <lucasapessoa@outlook.com>
2025-06-05 10:13:04 -0300

    corrige nomes de métodos e adiciona recuperação de plataformas e fotos no método get_product_placeholder_by_id

--- app/repositories/product_placeholder_repository.py
+++ app/repositories/product_placeholder_repository.py
@@ -2,14 +2,14 @@ from sqlmodel import Session, select
 from typing import List
 from uuid import UUID, uuid4
 
-from app.models.models import Product_Placeholder
+from app.models.models import Photo, Product_Placeholder
 from app.schemas.product_placeholder_schema import ProductPlaceholderCreate, ProductPlaceholderUpdate
 
 class ProductPlaceholderRepository:
     def __init__(self, session: Session):
         self.session = session
 
-    def create_product_placeholer(self, product_data: ProductPlaceholderCreate, metacritic_score: float) -> Product_Placeholder:
+    def create_product_placeholder(self, product_data: ProductPlaceholderCreate, metacritic_score: float) -> Product_Placeholder:
         product = Product_Placeholder(
             id=uuid4(),
             name=product_data.name.strip(),
@@ -26,8 +26,23 @@ class ProductPlaceholderRepository:
     def get_all_product_placeholer(self) -> List[Product_Placeholder]:
         return self.session.exec(select(Product_Placeholder)).all()
 
-    def get__product_placeholer_by_id(self, product_id: UUID) -> Product_Placeholder | None:
-        return self.session.get(Product_Placeholder, product_id)
+    def get_product_placeholder_by_id(self, id: UUID):
+        product = self.session.exec(
+            select(Product_Placeholder).where(Product_Placeholder.id == id)
+        ).one_or_none()
+
+        if product:
+            photos = self.session.exec(
+                select(Photo).where(Photo.product_id == id)
+            ).all()
+            product.photos = photos
+            
+            plataforms = self.session.exec(
+                select(Product_Placeholder.plataform).where(Product_Placeholder.id == id)
+            ).all()
+            product.plataforms = plataforms
+
+        return product
 
     def update_product_placeholer(self, product_id: UUID, product_data: ProductPlaceholderUpdate, metacritic_score: float) -> Product_Placeholder | None:
         product = self.session.get(Product_Placeholder, product_id)
