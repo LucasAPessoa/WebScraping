b8e110c558e95001e64a233ad9f9d31f919f3135
Lucas <lucasapessoa@outlook.com>
2025-06-05 10:12:56 -0300

    corrige nomes de funções no controlador de product_placeholder para consistência

--- app/controllers/product_placeholder_controller.py
+++ app/controllers/product_placeholder_controller.py
@@ -7,17 +7,17 @@ from app.schemas.product_placeholder_schema import (
 )
 from app.services.product_placeholder_services import ProductPlaceholderService
 
-def create_product_placeholer(data: ProductPlaceholderCreate, session: Session = Depends(get_session)):
-    return ProductPlaceholderService(session).create_product_placeholer(data)
+def create_product_placeholder(data: ProductPlaceholderCreate, session: Session = Depends(get_session)):
+    return ProductPlaceholderService(session).create_product_placeholder(data)
 
-def get_all_product_placeholer(session: Session = Depends(get_session)):
-    return ProductPlaceholderService(session).get_all_product_placeholer()
+def get_all_product_placeholder(session: Session = Depends(get_session)):
+    return ProductPlaceholderService(session).get_all_product_placeholder()
 
-def get_product_by_id(product_id: str, session: Session = Depends(get_session)):
+def get_product_placeholder_by_id(product_id: str, session: Session = Depends(get_session)):
     return ProductPlaceholderService(session).get_product_placeholder_by_id(product_id)
 
-def update_product(product_id: str, data: ProductPlaceholderUpdate, session: Session = Depends(get_session)):
-    return ProductPlaceholderService(session).update_product_placeholer(product_id, data)
+def update_product_placeholder(product_id: str, data: ProductPlaceholderUpdate, session: Session = Depends(get_session)):
+    return ProductPlaceholderService(session).update_product_placeholder(product_id, data)
 
-def delete_product(product_id: str, session: Session = Depends(get_session)):
-    return ProductPlaceholderService(session).delete_product_placeholer(product_id)
+def delete_product_placeholder(product_id: str, session: Session = Depends(get_session)):
+    return ProductPlaceholderService(session).delete_product_placeholder(product_id)
